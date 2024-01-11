379d4c2cd5d96d68c85d8d681a0cc67a36ac9af2
Mslonoff <matthew.slonoff@gmail.com>
2024-01-11 18:37:08 -0500

    alert and button

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import ListGroup from './components/ListGroup';
+import Button from './components/Button';
+import Alert from './components/Alert';
+
+
 
 function App() {
-  return <div><ListGroup /></div>;
+  return (
+    <div>
+      <Alert>My alert</Alert>
+      <Button color="primary" onClick={() => console.log('Clicked')}>My Button</Button>
+    </div>
+  );
 }
 
 export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,13 +1,20 @@
 import { useState } from "react";
 
-function ListGroup() {
-    let items = ['New York', 'San Francisco', 'Tokyo', 'London', 'Paris'];
+interface Props {
+    items: string[];
+    heading: string;
+    // (item: string) => void
+    onSelectItem: (item: string) => void;
+}
+
+function ListGroup({ items, heading, onSelectItem }: Props) {
+
     const [selectedIndex, setSelectedIndex] = useState(-1);
 
 
   return (
     <>
-        <h1>List</h1>
+        <h1>{heading}</h1>
         { items.length === 0 && <p>No item found</p> }
         <ul className="list-group">
             {items.map((item, index) => (
@@ -17,7 +24,10 @@ function ListGroup() {
                 : 'list-group-item'
             }
             key={item}
-            onClick={() => { setSelectedIndex(index); }}
+            onClick={() => {
+                setSelectedIndex(index);
+                onSelectItem(item);
+            }}
             >
                 {item}
                 </li>
